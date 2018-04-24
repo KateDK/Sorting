065520c85ac857e20b3c78a21d8cbdc191ad329c
Kate Dubitski <kate.dubitski@gmail.com>
2018-04-24 09:31:03 -0400

    addint updated bubbles code, missing internal loop

--- bubblesort.js
+++ bubblesort.js
@@ -1,47 +1,29 @@
-// const bubbleSort = array => {
-//  if (array.length <= 1) {
-//    return array;
-//  }
-
-//  for (var i = 0; i < array.length - 1; i++) {
-//    var element1 = array[i];
-//    var element2 = array[i + 1];
-
-
-//  }
-
-// }
-
-
-// class bubbleSort {
-//   constructor(array) {
-//   }
-
-//   swap() {
-//     // if (array.length <= 1) {
-//     //      return array;
-//     //    }
-
-//     //    for (var i = 0; i < array.length - 1; i++) {
-//     //      var element1 = array[i];
-//     //      var element2 = array[i + 1];
-
-//   }
-// }
-
-const bubbleSort = {};
-
-bubbleSort.swap = function(array) {
+'use etrict';
+const bubbleSort = array => {
   if (array.length <= 1) {
-       return array;
-     }
+    return array;
+  }
+  let arrayLength = array.length - 1;
+  for (let i = 0; i < arrayLength; i++) {
+    // let element1 = array[i];
+    // let element2 = array[i + 1];
+    if (!inOrder(array, i)) {
+      //some code
+      swap(i, i + 1, array);
+    }
+  }
+  return array;
+};
 
-     for (var i = 0; i < array.length - 1; i++) {
-       var element1 = array[i];
-       var element2 = array[i + 1];
-
-
-     }
+function inOrder(array, index) {
+  if (index === array.lenght - 1) return true;
+  return array[index] < array[index + 1];
 }
 
-console.log(bubbleSort.swap.calls());
+function swap(index1, index2, array) {
+  console.log('elm 1a', array[index1]);
+  console.log(array[index2]);
+  let firstElm = array[index1];
+  array[index1] = array[index2];
+  array[index2] = firstElm;
+}
